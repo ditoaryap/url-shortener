dfc9b495228865be4aed26d64fc7690ed4b4706b
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-08-18 19:10:20 +0700

    update

--- client/src/app/products/[productId]/page.tsx
+++ client/src/app/products/[productId]/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProductId = ({ params }: { params: { productId: string } }) => {
-  return <div>ProductId {params.productId} </div>;
+  return (
+    <div>
+      <h1>Detail About product {params.productId}</h1>
+    </div>
+  );
 };
 
 export default ProductId;
--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Product = () => {
-  return <div>Product HOME</div>;
+  return (
+    <div className="font-bold">
+      <h1>Product List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </div>
+  );
 };
 
 export default Product;
